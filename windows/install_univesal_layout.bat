d88ac6846b94cc20516f874019737789c54e7bb2
Nikita Shirokov <braindefender@gmail.com>
2019-02-08 18:38:41 +0700

    fixed windows installer

--- windows/install_univesal_layout.bat
+++ windows/install_univesal_layout.bat
@@ -1,4 +1,2 @@
-if not exist reg_layout.exe call make reg_layout.asm
-if not exist layouts\kbdunirus.dll call make layouts\kbdunirus.asm
-install layouts\kbdunirus.dll 07430419 00d0 "Uni RU" "Univesal Layout Russian"
-install layouts\kbdunieng.dll 07430409 00d1 "Uni EN" "Univesal Layout English"
+call .\install.bat .\layouts\kbdunirus.dll 07430419 00d0 "Uni RU" "Univesal Layout Russian"
+call .\install.bat .\layouts\kbdunieng.dll 07430409 00d1 "Uni EN" "Univesal Layout English"
