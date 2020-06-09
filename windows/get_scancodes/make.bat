ba963c2b58bfd16efaa1eba6b0a700c36bc0e679
Nikita Shirokov <braindefender@gmail.com>
2020-06-09 19:44:31 +0700

    Rebuild windows layout, update README

--- windows/get_scancodes/make.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-gcc -m32 -Wall -O2 -fno-ident -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables -flto -s get_scancodes.c -nostdlib -lkernel32 -luser32 -lshell32 -lmsvcrt -Wl,-e_xMain
--- windows/install_univesal_layout.bat
+++
@@ -1,2 +0,0 @@
-call .\install.bat .\layouts\kbdunirus.dll 07430419 00d0 "Uni RU" "Univesal Layout Russian"
-call .\install.bat .\layouts\kbdunieng.dll 07430409 00d1 "Uni EN" "Univesal Layout English"
